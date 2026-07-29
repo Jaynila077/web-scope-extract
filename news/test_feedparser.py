969f8c4594b15583d3a60b4657ddb0c8ae0017cf
Mahak753 <mahakgupta.mg.19aug@gmail.com>
2026-07-29 16:43:07 +0530

    Resolve merge conflicts in test_feedparser.py

--- news/test_feedparser.py
+++ news/test_feedparser.py
@@ -4,10 +4,6 @@ from datetime import datetime
 def monitor_rss_feed(feed_url, max_articles=5):
     print(f"Scanning RSS Feed: {feed_url}...\n")
     
-<<<<<<< HEAD
-
-=======
->>>>>>> df8f82196909e344ec1a9f318d0933db33801795
     feed = feedparser.parse(feed_url)
     
     if feed.bozo:
@@ -23,21 +19,12 @@ def monitor_rss_feed(feed_url, max_articles=5):
     print(f"Found {len(articles)} recent articles. Extracting data...\n")
     print("-" * 50)
 
-<<<<<<< HEAD
-
-    for index, article in enumerate(articles, start=1):
-      
-        title = article.get('title', 'No Title')
-        link = article.get('link', 'No Link')
-     
-=======
     for index, article in enumerate(articles, start=1):
        
         title = article.get('title', 'No Title')
         link = article.get('link', 'No Link')
         
        
->>>>>>> df8f82196909e344ec1a9f318d0933db33801795
         published = article.get('published', 'Unknown Time')
 
         print(f"Article {index}: {title}")
@@ -46,11 +33,7 @@ def monitor_rss_feed(feed_url, max_articles=5):
         print("-" * 50)
 
 if __name__ == "__main__":
-<<<<<<< HEAD
-
-=======
     
->>>>>>> df8f82196909e344ec1a9f318d0933db33801795
     TARGET_FEED = "http://feeds.bbci.co.uk/news/technology/rss.xml"
     
     # Run the function
